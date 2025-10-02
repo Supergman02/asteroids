acc30db124002455f890517b4eaca757a30ab096
Supergmam02 <griffith.a.disabato@gmail.com>
2025-10-02 19:15:33 -0400

    updated display to run at 60fps

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,6 +15,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
